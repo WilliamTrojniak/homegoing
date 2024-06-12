152777f0450fa3fb0ffe0c2b69bfb4b70c90bf0c
Will <william.trojniak@gmail.com>
2024-06-12 15:08:36 -0400

    mend
    dotmodules return their full src and dest paths

--- dotmanager/dotconfig.go
+++ dotmanager/dotconfig.go
@@ -2,6 +2,7 @@ package dotmanager
 
 import (
 	"os"
+	"path"
 
 	"github.com/BurntSushi/toml"
 )
@@ -15,8 +16,18 @@ type dotModuleData struct {
   // The path to the file to be linked. Relative to the dotfiles.toml config file
   Src string
 
-  // The name of the created link
-  Link string
+  // The path to the root to link, optionally specified in the config file
+  rootDst *string
+
+  // The path to the directory of where the config file was loaded from 
+  rootSrc *string
+
+  // The name of the module, optionally specified in the config file
+  Name string
+
+  // The filename of the created link, optionally specified in the config file
+  Target string
+
 }
 
 type dotModule struct {
@@ -24,7 +35,35 @@ type dotModule struct {
 }
 
 func (mod *dotModule) GetDest() string {
-  return os.ExpandEnv(mod.data.Dest);
+  dest := path.Join(os.ExpandEnv(mod.data.Dest), mod.getTarget());
+  if path.IsAbs(dest) {
+    return dest;
+  }
+  return path.Join(os.ExpandEnv(*mod.data.rootDst), dest);
+
+}
+
+func (mod *dotModule) GetSrc() string {
+  src := os.ExpandEnv(mod.data.Src);
+  if path.IsAbs(src) {
+    return path.Clean(src);
+  }
+  return path.Join(*mod.data.rootSrc, src);
+}
+
+func (mod *dotModule) GetName() string {
+  if len(mod.data.Name) == 0 {
+    return path.Base(mod.GetSrc());
+  } 
+
+  return mod.data.Name;
+}
+
+func (mod *dotModule) getTarget() string {
+  if len(mod.data.Target) == 0 {
+    return path.Base(mod.GetSrc());
+  }
+  return mod.data.Target;
 }
 
 type dotConfigData struct {
@@ -53,9 +92,20 @@ func (config *DotConfig) GetModules() []dotModule {
 }
 
 
-func ReadConfig(path string) (DotConfig, error) {
+func ReadConfig(filepath string) (DotConfig, error) {
+
+  // TODO Add check that path is absolute
+
   var config DotConfig;
-  _, err := toml.DecodeFile(path, &config.data);
+  _, err := toml.DecodeFile(filepath, &config.data);
+
+  srcPath := path.Dir(filepath);
+
+  for i, mod := range config.data.Modules {
+    mod.rootSrc = &srcPath;
+    mod.rootDst = &config.data.Dest 
+    config.data.Modules[i] = mod;
+  }
 
   return config, err;
 } 
